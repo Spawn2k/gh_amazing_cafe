31f0c95e7472708ac5614d739579484468fa23c8
Duy Tuan <duytuanvn1986@gmail.com>
2023-09-07 11:59:51 +0200

    final v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,43 @@
 (() => {
   // === DOM & VARS =======
   const DOM = {};
+  DOM.imgEls = document.querySelectorAll('.product-img');
+  console.log(DOM);
 
   // === INIT =============
-  const init = () => {};
+  const init = () => {
+    DOM.imgEls.forEach((img) => {
+      img.addEventListener('mousemove', onMouseMove);
+    });
+  };
 
   // === EVENTHANDLER =====
+
+  const onMouseMove = (e) => {
+    const { clientX } = e;
+    const imgEl = e.target;
+    console.log(imgEl);
+    const percent = calcPercent(clientX);
+
+    moveImageBackground(imgEl, percent);
+  };
 
   // === XHR/FETCH ========
 
   // === FUNCTIONS ========
+
+  const calcPercent = (clientX) => {
+    return (clientX / window.innerWidth) * 100;
+  };
+
+  const moveImageBackground = (img, percent) => {
+    img.animate(
+      {
+        objectPosition: `${percent}% 0%`,
+      },
+      { fill: 'forwards', duration: 250, easing: 'ease-in' }
+    );
+  };
 
   init();
 })();
